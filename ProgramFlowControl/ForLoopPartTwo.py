859ea4fbba81789ffcb472339a815f172b5c632a
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-05 11:43:09 -0700

    This program demonstrates separators in a string using for loop

---
+++ ProgramFlowControl/ForLoopPartTwo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+number = "9,223;372:036 854,775,:807"
+separators = ""
+
+for char in number:
+    if not char.isnumeric():
+        separators = separators + char
+
+print(separators)
+
+values = "".join(char if char not in separators else " " for char in number).split()
+print([int(val) for val in values])
