5653dac067e892514afdd00353372f85e7f289d7
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-05 09:42:55 -0700

    This program demonstrates boolean expressions

---
+++ ProgramFlowControl/booleanExpressions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+day = "Saturday"
+temperature = 30
+raining = True
+
+if (day == "Saturday" and temperature > 27) and not raining:
+    print("Go swimming")
+else:
+    print("Learn Python")
