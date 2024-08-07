6c4204d6ca91852b5441c72a27dc9f9f1a1904ca
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-06 18:08:54 -0700

    This program demonstrates the break statement in a while loop

---
+++ ProgramFlowControl/breakInAWhileLoop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+available_exits = ["north", "south", "east", "west"]
+
+chosen_exit = ""
+while chosen_exit not in available_exits:
+    chosen_exit = input("Please choose a direction: ")
+    if chosen_exit.casefold() == "quit":
+        print("Game over")
+        break
+
+
+print("aren't you glad you got out of there?")
