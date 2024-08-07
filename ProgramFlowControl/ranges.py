6e83864bfeecb4f3ad05b68cbeeb8d563217232a
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-08-06 17:09:37 -0700

    more range function practice

--- ProgramFlowControl/ranges.py
+++ ProgramFlowControl/ranges.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
 
-for i in range(1, 20):
+# for i in range(1, 20):
+#     print("i is now {}".format(i))
+
+
+# for i in range(10):
+#     print("i is now {}".format(i))
+
+
+# for i in range(0, 10, 2):
+#     print("i is now {}".format(i))
+
+for i in range(10, 0, -2):
     print("i is now {}".format(i))
